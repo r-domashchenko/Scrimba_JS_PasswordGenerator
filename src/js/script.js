884c842d6a380c9faf030f7c15cdaa3f9077493d
r-domashchenko <therostofan@gmail.com>
2022-11-24 19:57:54 +0200

    better code added (universal function plus())

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,21 @@
-let scoreLeft = 0,
-    scoreRight = 0;
+let Left = 0,
+    Right = 0,
+    scoreLeft = document.getElementById('scoreLeft'),
+    scoreRight = document.getElementById('scoreRight');
 
-
-function plusL(count) {
-    scoreLeft += count;
-    document.getElementById('scoreLeft').textContent = scoreLeft;
-    return scoreLeft;
+function plus(count, side) {
+    if (side === 'R') {
+        Right += count;
+        scoreRight.textContent = Right;
+    } else {
+        Left += count;
+        scoreLeft.textContent = Left;
+    }
 };
 
-function plusR(count) {
-    scoreRight += count;
-    document.getElementById('scoreRight').textContent = scoreRight;
-    return scoreRight;
+function newGame() {
+    Left = 0;
+    Right = 0;
+    scoreLeft.textContent = 0;
+    scoreRight.textContent = 0;
 };
